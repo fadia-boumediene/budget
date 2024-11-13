663e38df250acaae8a9aa004b07ea890c49201c3
fadia-boumediene <fadiaboumediene11@gmail.com>
2024-11-13 11:29:41 +0100

    up

--- budget/routes/web.php
+++ budget/routes/web.php
@@ -176,6 +176,7 @@ Route::controller(groupOperationController::class)->group(function(){
 Route::controller(opeartionController::class)->group(function(){
     Route::get('/testing/S_Action/{port}/{prog}/{sous_prog}/{act}/{s_act}', 'calculerEtEnvoyer');
    // Route::get('/testing/Action/{port}/{prog}/{sous_prog}/{act}', 'calculerEtEnvoyer');
+   Route::get('/testing/S_action/{s_act}/{T}', 'afficherDPIA');
 });
 
 //===============ROUTE SOUS OPERATION==============================
